81191567ba70c297a7e5fb0b315baa0c0761f1fb
user <user@asd18>
2020-06-09 09:49:39 +0900

    ClassExam 추가

--- src/Arr.java
+++ src/Arr.java
@@ -5,21 +5,21 @@ public class Arr
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
        
-       int[] arr = new int[5];
+       int arr[] = new int[5];
        int max = 0;
        int num = 0;
        
        
        for(int i=0; i<5; i++){
-       System.out.print(i+1 + "번 입력:");
+       System.out.print(i+1+"번 입력:");
        arr[i] = sc.nextInt();
        }
        
        for(int j=0; j<5; j++){
            
-           if(arr[j]>max){
+           if(max<arr[j]){
                max = arr[j];
-               num = j+1;
+               num+=1;
            }
        }
        
