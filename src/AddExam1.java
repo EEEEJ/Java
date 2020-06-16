42562290ba30e9236cb7471008ccb48a6efe5711
EEEEJ <64175118+EEEEJ@users.noreply.github.com>
2020-06-16 09:41:52 +0900

    Create AddExam1.java

---
+++ src/AddExam1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AddExam1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c = 0;
+		
+		System.out.print("시작 숫자: ");
+		a = sc.nextInt();
+		System.out.print("마지막 숫자: ");
+		b = sc.nextInt();
+		
+		for(int i=a; i<=b; i++) {
+			c+=i;
+		}
+		System.out.println(a + "부터 " + b + "까지의 합은 " + c);
+
+	}
+
+}
